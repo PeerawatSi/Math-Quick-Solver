c499b61ed27b042e704e5f2b677cf0e2f0cd526b
PeerawatSi <peerawat_si@cmu.ac.th>
2023-06-15 09:17:51 +0700

    make title function

---
+++ source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void title(string text);
+
+
+int main()
+{
+    title("Math-Quick-Solver!!!");
+
+    return 0;
+}
+
+void title(string text)
+{
+    cout << "----------------------------------------------------------\n";
+    cout << text << endl;
+    cout << "----------------------------------------------------------\n";
+}
